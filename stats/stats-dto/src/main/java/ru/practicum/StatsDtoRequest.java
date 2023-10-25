75cc541c4f1f3d451739e76d6d10577b4f0d2a48
Romangolodyuk25 <r.golodyuk@mail.ru>
2023-10-25 19:09:55 +0300

    Создал структуру проекта, добавил клиент для работы со статистикой приложения, добавил бд и эндроинты для работы со статистикой(фикс)

--- stats/stats-dto/src/main/java/ru/practicum/StatsDtoRequest.java
+++ stats/stats-dto/src/main/java/ru/practicum/StatsDtoRequest.java
@@ -1,9 +1,6 @@
 package ru.practicum;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.*;
 
 import javax.validation.constraints.NotNull;
 
@@ -11,6 +8,7 @@ import javax.validation.constraints.NotNull;
 @Setter
 @Builder
 @AllArgsConstructor
+@NoArgsConstructor
 public class StatsDtoRequest {
     @NotNull
     String app;
