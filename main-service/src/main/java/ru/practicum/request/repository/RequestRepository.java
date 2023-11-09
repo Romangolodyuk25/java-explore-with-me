275970b88e5eaae8e3bf30a53c9091f82e74c181
Romangolodyuk25 <r.golodyuk@mail.ru>
2023-11-09 17:48:47 +0300

    Работа с events, users, compilations, requests Добавил эндпоинты для работы с вышеперечисленными сущностями Добавил таблицы

--- main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
+++ main-service/src/main/java/ru/practicum/request/repository/RequestRepository.java
@@ -3,7 +3,6 @@ package ru.practicum.request.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import ru.practicum.Status;
 import ru.practicum.request.model.Request;
 
 import java.util.List;
