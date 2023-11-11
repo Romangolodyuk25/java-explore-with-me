596f5dc5dc7aefca802a97c631526bc3f66a299f
Romangolodyuk25 <r.golodyuk@mail.ru>
2023-11-11 19:23:49 +0300

    Исправление по итогам ревью, проверка рабты функциональности

--- stats/stats-dto/src/main/java/ru/practicum/compilation/UpdateCompilationRequest.java
+++ stats/stats-dto/src/main/java/ru/practicum/compilation/UpdateCompilationRequest.java
@@ -6,8 +6,6 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
 import java.util.List;
 
 @Getter
